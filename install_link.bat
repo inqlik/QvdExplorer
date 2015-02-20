70ea873f2449c5b8cb00baf248a732bb0bc96507
Vadim Tsushko <vadimtsushko@gmail.com>
2015-02-20 11:04:21 +0500

    Minor changes

--- install_link.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0
-REM copy QvdExplorer.lnk c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\
-cscript createLink.vbs "c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\QvdExplorer.lnk" "QvdExplorer.bat"
---
+++ setup.bat
@@ -0,0 +1,2 @@
+cd %~dp0
+cscript setup.vbs "c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\QvdExplorer.lnk" "QvdExplorer.bat"
