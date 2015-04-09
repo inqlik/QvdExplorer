e81e1e768b79c9d59ee3208395fcf2506113a1ed
Vadim Tsushko <vadimtsushko@gmail.com>
2015-04-09 15:12:23 +0500

    Remove old link

--- setup.bat
+++ setup.bat
@@ -1,2 +1,3 @@
 cd %~dp0
+IF EXIST "c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\QvdExplorer.lnk" DEL /F "c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\QvdExplorer.lnk"
 cscript setup.vbs "c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\QvdExplorer.lnk" "QvdExplorer.bat"
