0cbbf530da6af329d840348abc06fd397e478990
Vadim Tsushko <vadimtsushko@gmail.com>
2015-06-08 16:23:17 +0500

    Checking for various locations of SendTo folder

--- setup.bat
+++ setup.bat
@@ -1,3 +1,21 @@
+REM @ECHO OFF
 cd %~dp0
-IF EXIST "c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\QvdExplorer.lnk" DEL /F "c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\QvdExplorer.lnk"
-cscript setup.vbs "c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\QvdExplorer.lnk" "QvdExplorer.bat"
+
+IF EXIST "%systemdrive%\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\" SET sendToDir=c:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\
+IF [%sendToDir%] neq [] GOTO BODY
+
+IF EXIST "%systemroot%\Profiles\user\SendTo\" SET sendToDir=%systemroot%\Profiles\user\SendTo\
+IF [%sendToDir%] neq [] GOTO BODY
+
+IF EXIST "%systemdrive%\Documents and Settings\user\SendTo" SET sendToDir=%systemdrive%\Documents and Settings\user\SendTo
+IF [%sendToDir%] neq [] GOTO BODY
+
+GOTO FAIL
+
+:BODY
+IF EXIST "%sendToDir%QvdExplorer.lnk" DEL /F "%sendToDir%QvdExplorer.lnk"
+cscript setup.vbs "%sendToDir%QvdExplorer.lnk" "QvdExplorer.bat"
+EXIT
+:FAIL
+ECHO ERROR! Failed to find SendTo folder
+pause
